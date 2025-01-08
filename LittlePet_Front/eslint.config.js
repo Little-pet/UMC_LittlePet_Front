525910ce7e8b1ed68339046d1fb47cfd09f183af
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2025-01-08 11:16:02 +0900

    [feat]ESLint와 Prettier 설정

--- LittlePet_Front/eslint.config.js
+++ LittlePet_Front/eslint.config.js
@@ -1,28 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+  }, // 대상 파일 패턴
+  { languageOptions: { globals: globals.browser } }, // 브라우저 환경 글로벌 변수 설정
+  pluginJs.configs.recommended, // ESLint 추천 규칙
+  ...tseslint.configs.recommended, // TypeScript 추천 규칙
+  pluginReact.configs.flat.recommended, // React 추천 규칙
+  {
+    settings: {
+      react: {
+        version: 'detect', // React 버전 자동 감지
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      prettier: pluginPrettier,
     },
+    // Prettier 설정 포함
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'no-unused-vars': 'warn', // 사용하지 않는 변수에 대해 경고
+      'no-undef': 'warn', // 정의되지 않은 변수에 대해 경고
+      'react/prop-types': 'off', // PropTypes 사용 안 할 경우 비활성화
+      'react/react-in-jsx-scope': 'off', // React 17+에서는 필요 없음
+      'no-console': 'warn', // console.log 사용 경고
+      'prettier/prettier': 'error', // Prettier 규칙 위반 시 ESLint 오류로 표시
     },
   },
-)
+];
