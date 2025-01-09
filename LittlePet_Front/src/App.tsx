e92657d000d8b017bfefe805e5f5738c1f80eb20
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2025-01-10 01:44:37 +0900

    [feat]header 개발중

--- LittlePet_Front/src/App.tsx
+++ LittlePet_Front/src/App.tsx
@@ -1,7 +1,22 @@
 import './App.css';
+import logo from '@assets/logo.png';
 
 function App() {
-  return <></>;
+  return <header className="flex justify-between
+  items-center text-black py-6 px-8
+  md:px-32 bg-white drop-shadow-md">
+    <a href="#">
+      <img src={logo} alt="" className="w-52 
+      hover:scale-105 transition-all"/>
+    </a>
+
+    <ul className="hidden xl:flex items-center
+    gap-12 font-semibold text-base">
+      <li>로그인/회원가입</li>
+      <li>ABOUT</li>
+      <li>CONTACT</li>
+    </ul>
+  </header>
 }
 
 export default App;
