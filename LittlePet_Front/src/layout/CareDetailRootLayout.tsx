00de663a523062b7bd9b5d2ff5dd1bd2f0dd4724
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2025-02-19 11:51:50 +0900

    [fix]관리방법 Navbar 고정 테스트

--- LittlePet_Front/src/layout/CareDetailRootLayout.tsx
+++ LittlePet_Front/src/layout/CareDetailRootLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
